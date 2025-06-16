3fe75f57fed7e83aa9501b114338541f39afd0c2
noto-kohane-9932 <616-student008@la-study.com>
2025-06-16 17:57:56 +0900

    ファイル更新

--- src/main/java/la/dao/CategoriesDAO.java
+++ src/main/java/la/dao/CategoriesDAO.java
@@ -1,5 +1,17 @@
 package la.dao;
 
 public class CategoriesDAO {
+	private String url = "jdbc:postgresql:team_dev_pisuta_shop";
+	private String user = "student";
+	private String pass = "himitu";
 
+	public CategoriesDAO() throws DAOException {
+
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new DAOException("JDBCドライバの登録に失敗しました。");
+		}
+	}
 }
