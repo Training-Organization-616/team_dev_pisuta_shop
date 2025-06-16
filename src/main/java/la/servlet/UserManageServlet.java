23ac9c786b81978b985e0f702d60f756a9e429f3
noto-kohane-9932 <616-student008@la-study.com>
2025-06-16 11:50:05 +0900

    ファイル更新

--- src/main/java/la/servlet/UserManageServlet.java
+++ src/main/java/la/servlet/UserManageServlet.java
@@ -1,24 +1,59 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import la.bean.UserBean;
+import la.dao.UsersDAO;
+
 @WebServlet("/UserManageServlet")
 public class UserManageServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		try {
+			String action = request.getParameter("action");
+			UsersDAO dao = new UsersDAO();
+
+			if (action == null || action.length() == 0) {
+
+				List<UserBean> users = dao.findAll();
+				request.setAttribute("users", users);
+				gotoPage(request, response, "/adminUser.jsp");
+
+				//検索ボタン押下時（管理者画面）
+			} else if (action.equals("serch")) {
+				int userId = Integer.parseInt(request.getParameter("userId"));
+				UserBean user = dao.findUserById(userId);
+				request.setAttribute("user", user);
+				gotoPage(request, response, "/adminUserInfo.jsp");
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました。");
+			gotoPage(request, response, "/errInternal.jsp");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
+	}
+
+	private void gotoPage(HttpServletRequest request,
+			HttpServletResponse response, String page) throws ServletException,
+			IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
 	}
 
 }
--- src/main/webapp/adminUser.jsp
+++ src/main/webapp/adminUser.jsp
@@ -9,5 +9,43 @@
 </head>
 <body>
 
+<form action="/team_dev_pisuta_shop/UserManageServlet" method="post">
+<input type="text" name="userId" placeholder="会員番号">
+<button>検索</button>
+<input type="hidden" name="action" value="search">
+<br>
+</form>
+
+<table border="1">
+			<tr>
+				<th>会員番号</th>
+				<th>氏名</th>
+				<th>住所</th>
+				<th>電話番号</th>
+				<th>email</th>
+				<th>生年月日</th>
+			</tr>
+
+
+			<!--c:ifは数字を「仕事中」のように直すためのもの-->
+
+			<c:forEach items="${users}" var="user">
+				<tr>
+					<td>${user.id}</td>
+					<td>${user.name}</td>
+					<td>${user.address}</td>
+					<td>${user.tel}</td>
+					<td>${user.email}</td>
+					<td>${user.birthday}</td>
+					
+					
+				</tr>
+			</c:forEach>
+		</table>
+
+
+
+${list.id}
+
 </body>
 </html>
