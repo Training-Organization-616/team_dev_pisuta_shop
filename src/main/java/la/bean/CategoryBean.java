974ce522e61461deee0f27df290633ceaced7487
noto-kohane-9932 <616-student008@la-study.com>
2025-06-16 09:19:10 +0900

    CtegoryBean.java完成

--- src/main/java/la/bean/CategoryBean.java
+++ src/main/java/la/bean/CategoryBean.java
@@ -3,5 +3,24 @@ package la.bean;
 import java.io.Serializable;
 
 public class CategoryBean implements Serializable {
+	private int id;//カテゴリID
+	private String name;//カテゴリ名
+
+	public CategoryBean() {
+
+	}
+
+	public CategoryBean(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 }
