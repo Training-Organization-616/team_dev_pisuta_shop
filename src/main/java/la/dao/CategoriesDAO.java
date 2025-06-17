5f8ef4efed79bd07e010ad27d9e5d15e6050ac3c
noto-kohane-9932 <616-student008@la-study.com>
2025-06-17 09:45:08 +0900

    ファイル更新

--- src/main/java/la/dao/CategoriesDAO.java
+++ src/main/java/la/dao/CategoriesDAO.java
@@ -1,5 +1,15 @@
 package la.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import la.bean.CategoryBean;
+
 public class CategoriesDAO {
 	private String url = "jdbc:postgresql:team_dev_pisuta_shop";
 	private String user = "student";
@@ -12,6 +22,29 @@ public class CategoriesDAO {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			throw new DAOException("JDBCドライバの登録に失敗しました。");
+		}
+	}
+
+	//カテゴリ一覧表示
+	public List<CategoryBean> findAll() throws DAOException {
+		String sql = "select * from categories";
+		try (
+				Connection con = DriverManager.getConnection(url, user, pass);
+				PreparedStatement st = con.prepareStatement(sql);
+				ResultSet rs = st.executeQuery();) {
+
+			List<CategoryBean> list = new ArrayList<CategoryBean>();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+
+				CategoryBean bean = new CategoryBean(id, name);
+				list.add(bean);
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
 		}
 	}
 }
