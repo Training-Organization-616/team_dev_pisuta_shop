15397056799ffd462dc7bc9187dce5a819e3adc5
tanioka-yuuya-9941 <616-student017@la-study.com>
2025-06-17 09:46:53 +0900

    ListingServlet完成

--- src/main/java/la/servlet/ListingServlet.java
+++ src/main/java/la/servlet/ListingServlet.java
@@ -1,11 +1,20 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import la.bean.UserBean;
+import la.dao.CategoriesDAO;
+import la.dao.ConditionsDAO;
+import la.dao.ItemsDAO;
 
 /**
  * Servlet implementation class ListingServlet
@@ -13,27 +22,67 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ListingServlet")
 public class ListingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListingServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ListingServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			//文字エンコードセット
+			request.setCharacterEncoding("UTF-8");
+			//アクションパラメータ
+			String action = request.getParameter("action");
+			//DAOインスタンス
+			CategoriesDAO categoriesDao = new CategoriesDAO();
+			ConditionsDAO conditionsDao = new ConditionsDAO();
+			ItemsDAO itemsDao = new ItemsDAO();
+
+			if (Objects.isNull(action) || action.isEmpty()) {
+				gotoPage(request, response, "listing.jsp");
+			} else if (action.equals("add")) {
+				//入力情報の取得
+				String name = request.getParameter("name");
+				int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+				int price = Integer.parseInt(request.getParameter("price"));
+				int condId = Integer.parseInt(request.getParameter("condId"));
+				String comment = request.getParameter("comment");
+				//ログインユーザーの取得
+				HttpSession session = request.getSession();
+				UserBean user = (UserBean) session.getAttribute("user");
+				//出品処理
+				itemsDao.addItem(name, categoryId, user.getId(), price, condId, comment);
+
+				gotoPage(request, response, "profile.jsp");
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private void gotoPage(HttpServletRequest request,
+			HttpServletResponse response, String page) throws ServletException,
+			IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
