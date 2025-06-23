94b47653cadba003083097b833f2cd32e546ef0b
tanioka-yuuya-9941 <616-student017@la-study.com>
2025-06-23 10:13:20 +0900

    ListingServletの更新（エラー有）

--- src/main/java/la/servlet/ListingServlet.java
+++ src/main/java/la/servlet/ListingServlet.java
@@ -1,16 +1,20 @@
 package la.servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 import la.bean.CategoryBean;
 import la.bean.ConditionBean;
@@ -24,6 +28,7 @@ import la.dao.ItemsDAO;
  * Servlet implementation class ListingServlet
  */
 @WebServlet("/ListingServlet")
+@MultipartConfig
 public class ListingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -61,103 +66,129 @@ public class ListingServlet extends HttpServlet {
 			sessionDao.setAttribute("categories", categories);
 			sessionDao.setAttribute("conditions", conditions);
 
+			//ログインユーザーの取得
+			HttpSession session = request.getSession();
+			UserBean user = (UserBean) session.getAttribute("user");
+
+			//未ログインでアクセス時
+			if (Objects.isNull(user)) {
+				response.sendRedirect("/team_dev_pisuta_shop/ItemServlet");
+				return;
+			}
+
+			//Actionパラメータ
 			if (Objects.isNull(action) || action.isEmpty()) {
 
 				gotoPage(request, response, "listing.jsp");
 
 			} else if (action.equals("add")) {
 				//入力情報の取得
+				//出品商品名
 				String name = request.getParameter("name");
+				//指定カテゴリーID
 				int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+				//商品の状態ID
 				int condId = Integer.parseInt(request.getParameter("conditionId"));
+				//出品者コメント
 				String comment = request.getParameter("comment");
+				//価格
 				int price = 0;
 
+				//商品名が無い場合
 				if (name == null || name.length() == 0) {
 					request.setAttribute("message", "商品名は必須です");
 					gotoPage(request, response, "listing.jsp");
 
-				} else if (name.length() >= 100) {
+				}
+				//商品名が１００文字以上の場合
+				else if (name.length() >= 100) {
 					request.setAttribute("message", "商品名は100文字以下にしてください");
 					gotoPage(request, response, "listing.jsp");
-
-				} else if (action.equals("edit")) {
-
-					// 商品　情報変更画面への遷移
-					gotoPage(request, response, "/editItem.jsp");
-
-					//商品　情報の更新
-					int id = Integer.parseInt(request.getParameter("id"));
-					//	String name = request.getParameter("name");
-					//	int categoryId = Integer.parseInt(request.getParameter("categoryId"));
-					int sellerId = Integer.parseInt(request.getParameter("sellerId"));
-					//	int price = Integer.parseInt(request.getParameter("price"));
-					//	int condId = Integer.parseInt(request.getParameter("condId"));
-					//	String comment = request.getParameter("comment");
-					String fileName = request.getParameter("fileName");
-
-					//入力チェック
-					if (Objects.isNull(id) || id == 0 || name == null || name.length() == 0
-							|| Objects.isNull(categoryId) || categoryId == 0 || Objects.isNull(sellerId)
-							|| sellerId == 0
-							|| Objects.isNull(price) || price == 0 || Objects.isNull(condId) || condId == 0
-							|| comment == null || comment.length() == 0 || fileName == null || fileName.length() == 0) {
-						request.setAttribute("message", "全ての項目を入力してください");
-						gotoPage(request, response, "/editItem.jsp");
-					} else if (name.length() > 100) {
-						request.setAttribute("name", "商品名は100文字以下にしてください");
-						gotoPage(request, response, "/editUser.jsp");
-					} else {
-						ItemsDAO dao = new ItemsDAO();
-						dao.updateItem(id, name, categoryId, price, condId, comment);
-
-						gotoPage(request, response, "/profile.jsp");
-					} //商品の削除処理
-				} else if (action.equals("delere")) {
-					// セッションから商品IDの取得
-					HttpSession session = request.getSession(false);
-					ItemBean item = (ItemBean) session.getAttribute("item");
-					int id = item.getId();
-
-					ItemsDAO dao = new ItemsDAO();
-					dao.deleteItemById(id);
-
-					ItemsDAO itemsDao1 = new ItemsDAO();
-					itemsDao1.deleteItemById(id);
-
-					// セッションの削除
-					if (session != null) {
-						session.removeAttribute("item");
-					}
-
-					response.sendRedirect("/team_dev_pisuta_shop/ItemServlet");
 				}
 
-			} else {
-				try {
-					int price = Integer.parseInt(request.getParameter("price"));
-					int id = Integer.parseInt(request.getParameter("id"));
-					String name = request.getParameter("name");
-					int categoryId = Integer.parseInt(request.getParameter("categoryId"));
-					int sellerId = Integer.parseInt(request.getParameter("sellerId"));
-					int price1 = Integer.parseInt(request.getParameter("price"));
-					int condId = Integer.parseInt(request.getParameter("condId"));
-					String comment = request.getParameter("comment");
-					String fileName = request.getParameter("fileName");
+				//画像ファイルの受け取り
+				Part part = request.getPart("product_image");
+				//ファイル名を取得
+				String fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+				//アップロードするフォルダ
+				String path = getServletContext().getRealPath("/upload");
+				System.out.println(path + File.separator + fileName);
+				//書き込み
+				part.write(path + File.separator + fileName);
 
-					//ログインユーザーの取得
-					HttpSession session = request.getSession();
-					UserBean user = (UserBean) session.getAttribute("user");
-					//出品処理
-					itemsDao.addItem(name, categoryId, user.getId(), price1, condId, comment);
+				//データベース登録
+				//itemsDao.addItem(name, categoryId, user.getId(), price, condId, comment, fileName);
 
-					gotoPage(request, response, "profile.jsp");
+				//登録した商品のidを検索
+				ItemBean itemBean;
 
-				} catch (Exception e) {
-					request.setAttribute("message", "価格は必須です");
-					gotoPage(request, response, "listing.jsp");
+				//ファイル名の変更
+				//現在のファイルのパス
+				File currentFile = new File(path + File.separator + fileName);
+				//新しいファイル名
+				File newFileName = new File("pict" + itemBean.getId() + ".png");
+
+				//ファイル名の変更
+				boolean success = currentFile.renameTo(newFileName);
+
+				if (!success) {
+					request.setAttribute("message", "内部エラーが発生しました。");
+					gotoPage(request, response, "/errInternal.jsp");
 					return;
 				}
+
+				//変更後データベースの更新
+
+				gotoPage(request, response, "profile.jsp");
+
+			} else if (action.equals("edit")) {
+
+				// 商品情報変更画面への遷移
+				gotoPage(request, response, "/editItem.jsp");
+
+				//商品情報の更新
+				int id = Integer.parseInt(request.getParameter("id"));
+				String name = request.getParameter("name");
+				int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+				int sellerId = Integer.parseInt(request.getParameter("sellerId"));
+				int price = Integer.parseInt(request.getParameter("price"));
+				int condId = Integer.parseInt(request.getParameter("condId"));
+				String comment = request.getParameter("comment");
+
+				//入力チェック
+				if (Objects.isNull(id) || id == 0 || name == null || name.length() == 0
+						|| Objects.isNull(categoryId) || categoryId == 0 || Objects.isNull(sellerId)
+						|| sellerId == 0
+						|| Objects.isNull(price) || price == 0 || Objects.isNull(condId) || condId == 0
+						|| comment == null || comment.length() == 0) {
+					request.setAttribute("message", "全ての項目を入力してください");
+					gotoPage(request, response, "/editItem.jsp");
+				} else if (name.length() > 100) {
+					request.setAttribute("name", "商品名は100文字以下にしてください");
+					gotoPage(request, response, "/editUser.jsp");
+				} else {
+					ItemsDAO dao = new ItemsDAO();
+					dao.updateItem(id, name, categoryId, price, condId, comment);
+
+					gotoPage(request, response, "/profile.jsp");
+				} //商品の削除処理
+			} else if (action.equals("delete")) {
+				// セッションから商品IDの取得
+				session = request.getSession(false);
+				ItemBean item = (ItemBean) session.getAttribute("item");
+				int id = item.getId();
+
+				ItemsDAO dao = new ItemsDAO();
+				dao.deleteItemById(id);
+
+				ItemsDAO itemsDao1 = new ItemsDAO();
+				itemsDao1.deleteItemById(id);
+
+				// セッションの削除
+				if (session != null) {
+					session.removeAttribute("item");
+				}
+				response.sendRedirect("/team_dev_pisuta_shop/ItemServlet");
 			}
 
 		} catch (Exception e) {
@@ -165,7 +196,6 @@ public class ListingServlet extends HttpServlet {
 			request.setAttribute("message", "内部エラーが発生しました。");
 			gotoPage(request, response, "/errInternal.jsp");
 		}
-		// TODO: handle exception
 	}
 
 	private void gotoPage(HttpServletRequest request,
