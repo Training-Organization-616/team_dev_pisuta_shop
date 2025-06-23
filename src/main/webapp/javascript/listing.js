7e1f52b2cbc78cf489bd8669e5092f1e610cc213
tanioka-yuuya-9941 <616-student017@la-study.com>
2025-06-23 17:41:45 +0900

    プレビュー表示追加

--- src/main/webapp/javascript/listing.js
+++ src/main/webapp/javascript/listing.js
@@ -1,3 +1,17 @@
 /**
  * 
  */
+
+window.addEventListener('load',function(){
+	const imageElm = document.querySelector("#preview");
+	const inputElm = document.querySelector("#import_image");
+	
+	inputElm.addEventListener('change',event => {
+		const file = event.target.files[0];
+		const reader = new FileReader();
+		reader.onload = () =>{
+			imageElm.src = reader.result;
+		};
+		reader.readAsDataURL(event.target.files[0]);
+	})
+})
